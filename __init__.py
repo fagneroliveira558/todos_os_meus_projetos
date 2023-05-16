475a7a418d9827c814d9365ecde19f529bfc7698
Fagner <fagneroliveira558@gmail.com>
2023-05-16 17:35:22 -0300

    'atualizando

---
+++ __init__.py
@@ -0,0 +1,7 @@
+numeroSecreto = 45
+chute = int(input('Adivinhe meu número secreto: '))
+
+if (numeroSecreto == chute):
+    print('Uau... Acertou meu número secreto! ')
+else:
+    print('Puts... Você não acertou meu número secreto!! ')
