64dcb49f8d888a2adfd0783ed3b96c58bc6f17f5
Fagner <fagneroliveira558@gmail.com>
2023-05-16 18:40:33 -0300

    Criando um novo arquivo.

---
+++ main.py
@@ -0,0 +1,4 @@
+lista_de_frutas = ['Banana', 'Pera', 'Laranja', 'Goiaba', 'Mexerica', 'Carambola', 'Tomate', 'Melão', 'Mamão', 'Manga', 'Melancia', 'Amecha', 'Pêcego']
+
+for frutas in lista_de_frutas:
+    print(frutas)
