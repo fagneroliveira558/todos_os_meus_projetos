a205980337a0300224cf317d1b1da58682df484e
Fagner <fagneroliveira558@gmail.com>
2023-05-17 00:38:14 -0300

    Alimentando os arquivos e, aplicando testes

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+print('*'*40)
+print('Seja muito bem vindo ao jogo de adivinhar... ')
+print('*'*40)
 numeroSecreto = 45
 chute = int(input('Adivinhe meu número secreto: '))
 
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 lista_de_frutas = ['Banana', 'Pera', 'Laranja', 'Goiaba', 'Mexerica', 'Carambola', 'Tomate', 'Melão', 'Mamão', 'Manga', 'Melancia', 'Amecha', 'Pêcego']
-
+lista_de_nomes = ['Fagner Araujo', 'RuteNeia Araujo', 'Kamila Araujo', 'Zoe Miwa Araujo', 'Miguel Araujo', 'Alícia Araujo']
 for frutas in lista_de_frutas:
     print(frutas)
+print('Abaixo, segue uma lista de nomes... ')
+
+for minha_familia in lista_de_nomes:
+    print(minha_familia)
